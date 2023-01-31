15d56aaad1e25ac31cf728d4d21ef8754e9ed732
Joynal Dsouza <joynaldsouza2@gmail.com>
2023-01-31 21:20:17 +0530

    added react-native-rolling-number-ticker package

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import RollingNumberTicker from 'react-native-rolling-number-ticker';
 
 const App = () => {
   return (
@@ -9,6 +10,12 @@ const App = () => {
         <View>
           <Text>Hello World</Text>
           <Text>Yayyy!!!</Text>
+          <RollingNumberTicker
+            textSize={20}
+            fromNumber={12400}
+            number={12929}
+            duration={1000}
+          />
         </View>
       </ScrollView>
     </>
