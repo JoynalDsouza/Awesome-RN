41cbc4924f358d43085c802772c576fc8bc5317b
Joynal Dsouza <joynaldsouza2@gmail.com>
2023-02-26 19:10:49 +0530

    add navigation to the app

--- App.js
+++ App.js
@@ -1,63 +1,20 @@
 import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-import RollingNumberTicker from 'react-native-rolling-number-ticker';
-import {formatIndianCurrency} from './src/utils/utilityfuctions';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import AnimationScreen from './src/screens/AnimationScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [text, setText] = useState('');
   return (
-    <>
-      <SafeAreaView />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text>Hello World</Text>
-          <Text>Yayyy!!!</Text>
-          <RollingNumberTicker
-            textSize={50}
-            fromNumber={10400}
-            number={12929}
-            duration={5000}
-            animationStartDelay={3000}
-          />
-        </View>
-
-        <View>
-          <TextInput
-            style={{
-              flex: 1,
-              backgroundColor: 'grey',
-              margin: 30,
-            }}
-            value={text}
-            onChangeText={value => {
-              let regex_num = value.replace(/^0+/, '');
-              setText(regex_num);
-            }}></TextInput>
-        </View>
-
-        <View>
-          <Text>{formatIndianCurrency('2210.111', true)}</Text>
-          <Text>{formatIndianCurrency(2210.111)}</Text>
-
-          <Text>
-            {Number('32932.378687').toLocaleString('en-IN', {
-              style: 'currency',
-              currency: 'INR',
-            })}
-          </Text>
-        </View>
-      </ScrollView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Animation" component={AnimationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
