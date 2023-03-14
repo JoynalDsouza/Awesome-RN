5a218a61798190806408493db395ca11c10b29b0
Joynal Dsouza <joynaldsouza2@gmail.com>
2023-03-14 21:49:23 +0530

    interpolate interpolated value

--- src/screens/AnimationScreen2.js
+++ src/screens/AnimationScreen2.js
@@ -4,10 +4,21 @@ import {View, Text, Animated, TouchableOpacity} from 'react-native';
 const AnimationScreen2 = () => {
   const scale = useRef(new Animated.Value(1)).current;
   const bgColorAnimation = useRef(new Animated.Value(0)).current;
+  const translateAnimation = useRef(new Animated.Value(0)).current;
 
   const backgroundColorInterpolate = bgColorAnimation.interpolate({
     inputRange: [0, 1],
     outputRange: ['red', 'green'],
+  });
+
+  const translateXInterpolate = translateAnimation.interpolate({
+    inputRange: [0, 1, 2],
+    outputRange: [0, 50, 0],
+  });
+
+  const opacityInterpolate = translateXInterpolate.interpolate({
+    inputRange: [0, 50],
+    outputRange: [1, 0],
   });
 
   const startAnimation = () => {
@@ -23,6 +34,16 @@ const AnimationScreen2 = () => {
         useNativeDriver: false,
       }),
     ]).start();
+  };
+
+  const startAnimation2 = () => {
+    Animated.timing(translateAnimation, {
+      duration: 1000,
+      toValue: 2,
+      useNativeDriver: true,
+    }).start(() => {
+      translateAnimation.setValue(0);
+    });
   };
   return (
     <>
@@ -42,6 +63,23 @@ const AnimationScreen2 = () => {
 
         <TouchableOpacity onPress={() => startAnimation()}>
           <Text>Start Animation</Text>
+        </TouchableOpacity>
+
+        <Animated.View
+          style={{
+            width: 100,
+            height: 100,
+            backgroundColor: 'yellow',
+            justifyContent: 'center',
+            alignItems: 'center',
+            transform: [{translateX: translateXInterpolate}],
+            opacity: opacityInterpolate,
+            // backgroundColor: backgroundColorInterpolate,
+          }}>
+          <Text style={{color: 'black'}}>Hello</Text>
+        </Animated.View>
+        <TouchableOpacity onPress={() => startAnimation2()}>
+          <Text>Start Animation2</Text>
         </TouchableOpacity>
       </View>
     </>
