2e8ab2db7914541dd180589bc0a5d5770aa7835c
Joynal Dsouza <joynaldsouza2@gmail.com>
2024-01-23 12:30:01 +0530

    remove code

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
-import AnimationScreen from './src/screens/AnimationScreen';
-import AnimationScreen2 from './src/screens/AnimationScreen2';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +10,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Animation" component={AnimationScreen} />
-        <Stack.Screen name="Animation2" component={AnimationScreen2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -10,79 +10,11 @@ import {
   View,
   Animated,
 } from 'react-native';
-import RollingNumberTicker from 'react-native-rolling-number-ticker';
-
-import {RatingSlider} from '../components/Slider2';
-import SliderX from '../components/SliderX';
-
-import SliderSvg from '../components/svg/SliderSvg';
-import {formatIndianCurrency} from '../utils/utilityfuctions';
 
 const HomeScreen = ({navigation}) => {
-  const [text, setText] = useState('');
-  const backgroundAnimated = useRef(new Animated.Value(0)).current;
-
-  const backgroundColor = backgroundAnimated.interpolate({
-    inputRange: [0, 3000],
-    outputRange: ['rgb(145,34,26)', 'rgb(234,234,23)'],
-  });
   return (
     <>
       <SafeAreaView />
-      <Animated.ScrollView>
-        <View>
-          <Text>Hello World</Text>
-          <Text>Yayyy!!!</Text>
-          <RollingNumberTicker
-            textSize={50}
-            fromNumber={10400}
-            number={12929}
-            duration={5000}
-            animationStartDelay={3000}
-          />
-        </View>
-
-        <RatingSlider />
-        <SliderSvg />
-        <View>
-          <TextInput
-            style={{
-              flex: 1,
-              backgroundColor: 'grey',
-              margin: 30,
-            }}
-            value={text}
-            onChangeText={value => {
-              let regex_num = value.replace(/^0+/, '');
-              setText(regex_num);
-            }}></TextInput>
-        </View>
-
-        <View>
-          <Text>{formatIndianCurrency('2210.111', true)}</Text>
-          <Text>{formatIndianCurrency(2210.111)}</Text>
-          <SliderX />
-          <Text>
-            {Number('32932.378687').toLocaleString('en-IN', {
-              style: 'currency',
-              currency: 'INR',
-            })}
-          </Text>
-        </View>
-        <View style={styles.container}>
-          <TouchableOpacity onPress={() => navigation.navigate('Animation')}>
-            <Text>Go to Animation</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.container}>
-          <TouchableOpacity onPress={() => navigation.navigate('Animation2')}>
-            <Text>Go to Animation2</Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={[styles.content, {backgroundColor: backgroundColor}]}></View>
-      </Animated.ScrollView>
     </>
   );
 };
